4b454fb7f1bcf5d15b31d607677871e6c074eae2
hieunguyen0802 <hieu.h.t.nguyen@tcu.edu>
2024-06-13 18:20:16 +0700

    start working on product screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,26 +1,50 @@
-import { StyleSheet, Text, View} from 'react-native';
+import Colors from '@/src/constants/Colors';
+import { StyleSheet, Text, View, Image, ScrollView} from 'react-native';
+import products from '@/assets/data/products';
 
-export default function TabOneScreen() {
+const ProductListItem = ({product}) => {
+  
   return (
-    <View className="align-center items-center flex-1 bg-black">
-      <Text>hihi</Text>
-    </View>
+    <View style={styles.container}>
+    <Image source = {{uri: product.image}} style={styles.image}/>
+    <Text style={styles.title}>{product.name}</Text>
+    <Text style={styles.title}>{product.price}</Text>
+  </View>
+  )
+}
+
+export default function MenuScreen() {
+  return (
+   <ScrollView>
+    <ProductListItem product={products[0]}/>
+    <ProductListItem  product={products[1]}/>
+   </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
+    borderColor: 'red',
+    borderWidth: 1
+
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 18,
+    fontWeight: '600',
+    marginVertical: 10
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold"
   },
+
+  image : {
+    width: '100%',
+    aspectRatio: 1
+  }
+  
 });
